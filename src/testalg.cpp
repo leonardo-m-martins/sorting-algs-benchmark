441f13303ea70baad9ac1aa2402c927b23f6179f
Leonardo Martins Martins <leonardo.mmartins@outlook.com>
2026-05-17 23:27:28 -0300

    feat: adiciona print parcial de vetor em testalg
    O print parcial é usado para verificar se um array foi alterado
    indevidamente pelo algoritmo.

--- src/testalg.cpp
+++ src/testalg.cpp
@@ -15,19 +15,32 @@ using namespace benchmark_utils;
 
 void print_is_sorted(std::vector<int> &v, std::string msg) {
     std::string sort_status = is_sorted(v) ? "Ordenado" : "Nao ordenado";
-    std::cout << std::left << std::setw(20) << msg + ":" << sort_status << std::endl;
+    std::cout << std::left << std::setw(20) << msg + ":" << std::setw(20) << sort_status << "\t";
+}
+
+void print_vector(std::vector<int> &v) {
+    for (size_t i = 0; i < 5; i++) {
+        std::cout << v[i] << ", ";
+    }
+    std::cout << "[...] ";
+    for (size_t i = v.size() - 6; i < v.size(); i++) {
+        std::cout << v[i] << ", ";
+    }    
+    std::cout << std::endl;
 }
 
 void test_alg(sort_algorithm sort_function, std::string func_name, std::vector<int> &v) {
     std::vector<int> vector_copy = v;
     sort_function(vector_copy);
     print_is_sorted(vector_copy, func_name);
+    print_vector(vector_copy);
 }
 
 int main() {
     int N = 10000;
     auto rd_vector = generate_random_vector(N);
     print_is_sorted(rd_vector, "Vetor randomico");
+    print_vector(rd_vector);
 
     test_alg(heap_sort, "Heap Sort", rd_vector);
     test_alg(selection_sort, "Selection Sort", rd_vector);
